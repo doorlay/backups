3f1f973ef76fe6c9bbcbc8fdf7a6c4bac5a0b622
Nicholas Doorlay <nicholas@doorlay.com>
2026-02-22 17:00:30 -0800

    Add backup capability, add script for Obsidian

--- sync/obsidian.sh
+++ sync/obsidian.sh
@@ -1,10 +1,10 @@
 DAY="$(date +%F)"
-BACKUPROOT="/srv/backups/.rsync-history/proton"
+BACKUPROOT="/srv/backups/.rsync-history/obsidian"
 BACKUPDEST="$BACKUPROOT/$DAY"
 
 ssh admin@192.168.1.216 "mkdir -p '$BACKUPDEST'"
 
 rsync -a --delete --backup --backup-dir="$BACKUPDEST" --exclude-from="./.exclusions" --itemize-changes \
   -e "ssh" \
-  "/Users/doorlay/Library/CloudStorage/ProtonDrive-nicholas@doorlay.com-folder/Offline/" \
-  "admin@192.168.1.216:/srv/backups/proton/"
+  "/Users/doorlay/Documents/Notes/" \
+  "admin@192.168.1.216:/srv/backups/obsidian/"
